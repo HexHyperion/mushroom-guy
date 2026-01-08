85c5bd6c6f93b657a51830bf4ccf57b457d1f4e2
HexHyperion <szymonjan@gmail.com>
2026-01-08 12:19:28 +0100

    Add background and center the guy

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const guys = [
   [
-    "_   \\◐_◑/    ",
+    "_   \\◐_◑/   ",
     " ||  | |     ",
     " |===|_|___  ",
     "          || ",
@@ -14,11 +14,11 @@ const guys = [
   //   "_||       ||_"
   // ],
   [
-    "    \\◐_◑/   _ ",
+    "    \\◐_◑/   _",
     "     | |  || ",
     "  ___|_|===| ",
-    " ||            ",
-    "_||            "
+    " ||          ",
+    "_||          "
   ],
   // [
   //   "    \\◐_◑/    ",
@@ -36,3 +36,8 @@ setInterval(() => {
   guySpace.textContent = guys[currentIndex++].join("\n");
   currentIndex %= guys.length;
 }, 300);
+
+let counter = 0;
+setInterval(() => {
+  document.querySelector("body").style.background = `conic-gradient(from ${counter++}deg, rgba(255,0,0,1) 0%, rgba(255,154,0,1) 10%, rgba(208,222,33,1) 20%, rgba(79,220,74,1) 30%, rgba(63,218,216,1) 40%, rgba(47,201,226,1) 50%, rgba(28,127,238,1) 60%, rgba(95,21,242,1) 70%, rgba(186,12,248,1) 80%, rgba(251,7,217,1) 90%, rgba(255,0,0,1) 100%)`
+}, 1)
